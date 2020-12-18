afc339ec12a82bf11b18fc370d249b357cba0d82
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-18 14:35:35 -0800

    Corrected UserModelTestCase docstring. Modified test_user_model.py.

--- test_user_model.py
+++ test_user_model.py
@@ -31,7 +31,7 @@ db.create_all()
 
 
 class UserModelTestCase(TestCase):
-    """Test views for messages."""
+    """Test User model."""
 
     def setUp(self):
         """Create test client, add sample data."""
